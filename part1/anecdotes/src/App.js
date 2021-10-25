cb3e6354e075b8f9e6a6aa578573a669162ee57d
chuazhihong <chuazhihong@hotmail.com>
2021-10-25 15:46:21 +0800

    completed-unicafe-and-anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,62 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// Header takes care of rendering the name of the course
-const Header = (props) => {
-	console.log(props)
-	return <h1>{props.course}</h1>
-}
+const Button = ({action,text}) => (
+	<button onClick={action}> {text} </button>
+)
 
-
-// Part renders the parts and number of exercises
-const Part = (props) => {
-	// console.log(props)
-	return <p>{props.part} {props.exercises}</p>
-}
-
-// Content prints content
-const Content = (props) => {
-	console.log(props)
-	
-	return (
-	  <div>
-	    <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-	    <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-	    <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-	  </div>
-)}
-
-// Total renders total number of exercises
-const Total = (props) => {
-	return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
-}
-
-// App is the root that renders the App
 const App = () => {
-	const course = {
-		name: 'Half Stack application development',
-		parts: [
-		  {
-			name: 'Fundamentals of React',
-			exercises: 10
-		  },
-		  {
-			name: 'Using props to pass data',
-			exercises: 7
-		  },
-		  {
-			name: 'State of a component',
-			exercises: 14
-		  }
-		]
-	  }
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+  const min = 0
+  const max = anecdotes.length
+  const [votes, setVotes] = useState(Array(max).fill(0))
 
-  return (
-   <div>
-	<Header course={course.name} />
-	<Content parts={course.parts} />
-	<Total parts={course.parts} />
-   </div>
-  )
+  const random = () => {
+	  return Math.floor(Math.random() * max + min)
+  }
+  
+  const next = () => {
+	  setSelected(random)
+  }
+
+  const vote = () => {
+	const newVotes = [...votes]
+	newVotes[selected] += 1
+	setVotes(newVotes)
+  }
+
+  const top = votes.indexOf(Math.max(...votes))
+
+	return (
+		<div>
+		<h1>Anecdote of the Day</h1>
+		<p>{anecdotes[selected]}</p>
+		<p>{"has "+votes[selected]+" votes"}</p>
+		<Button action={vote} text="vote"/>
+		<Button action={next} text="next anecdote"/>
+		<h1>Anecdote with Most Votes</h1>
+		<p>{anecdotes[top]}</p>
+		<p>{"has "+votes[top]+" votes"}</p>
+		</div>
+	)
 }
 
 export default App
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,39 +1,47 @@
 import React, { useState } from 'react'
 
-const Statistics = ({text,value}) => {
+const StatisticLine = ({text,value}) => {
 	if (text==='positive'){
-		return(
-			<p>{text} {value} %</p>
-		)
+		return(<tr><td>{text} {value} %</td></tr>)
 	}
-	return (
-		<p>{text} {value}</p>
-	)
-  }
+	return(<tr><td>{text} {value}</td></tr>)
+}
 
-const StatisticDisplay = (good,neutral,bad) => {
-	console.log(good)
+const Statistics = ({good,neutral,bad}) => {
 	const total = good + neutral + bad
 	const average = (-1*bad + 1*good)/total
 	const positive = good / total * 100
+
 	if (total===0){
-		return(
-			'No feedback given'
-		)
+		return(<tbody><tr><td>{"No feedback given"}</td></tr></tbody>)
 	}
-	
+	return(
+		<tbody>
+		  <StatisticLine text="good" value={good} />
+		  <StatisticLine text="neutral" value={neutral} />
+		  <StatisticLine text="bad" value={bad} />
+		  <StatisticLine text="all" value={total} />
+		  <StatisticLine text="average" value={average} />
+		  <StatisticLine text="positive" value={positive} />
+		</tbody>
+	  )
+  }
+
+const StatisticDisplay = ({good,neutral,bad}) => {
 	return (
-	<div>
-	<Statistics text="good" value={good} />
-	<Statistics text="neutral" value={neutral} />
-	<Statistics text="bad" value={bad} />
-	<Statistics text="all" value={total} />
-	<Statistics text="average" value={average} />
-	<Statistics text="positive" value={positive} />
-	</div>
+	<table>
+		<Statistics good={good} neutral={neutral} bad={bad} />
+	</table>
 	  )
 }
 
+const Button = (props) => (
+	<button onClick={props.handleClick}>
+		{props.text}
+	</button>
+)
+
+// SEE TOPIC: Conditional Rendering and Event Handling Revisited
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -44,17 +52,11 @@ const App = () => {
     <div>
 	  <h1>{"give feedback"}</h1>
       
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-	  <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
+	  <Button handleClick={() => setGood(good + 1)} text="good" />
+	  <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+	  <Button handleClick={() => setBad(bad + 1)} text="bad" />
       
-	  <h1>{"Statistics"}</h1>
+	  <h1>{"statistics"}</h1>
 	  <StatisticDisplay good={good} neutral={neutral} bad={bad}/>
 	  
     </div>
